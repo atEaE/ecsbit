edefa0f59a047cdd8245ed9efda6c49fce671658
Aoki Eta <imposter.kore2626@gmail.com>
2025-03-01 23:17:29 +0900

    feat: statsにjson出力のメソッドを用意

--- stats/stats.go
+++ stats/stats.go
@@ -1,5 +1,27 @@
 package stats
 
+import "encoding/json"
+
+// ToJSON : Worldの統計情報をJSON形式に変換します
+func ToJSON(stats World) []byte {
+	b, err := json.Marshal(stats)
+	// worldが生成したものを表示することになるので、基本的にエラーは発生しないはず
+	// なので、使う側がエラー処理する手間を省くためにpanicで処理している
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// ToJSONPretty : Worldの統計情報を整形されたJSON形式に変換します
+func ToJSONPretty(stats World) []byte {
+	b, err := json.MarshalIndent(stats, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
 // World : Worldの統計情報
 type World struct {
 	Entities Entities `json:"entities"` // Entityの統計情報
